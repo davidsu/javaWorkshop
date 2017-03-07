82da9697f649ae27c4ac2e9bfb17522a8dcfeb28
David <davidsu@wix.com>
2017-03-07 23:12:34 +0200

    improved system login ui

--- client/src/components/systemLogin.jsx
+++ client/src/components/systemLogin.jsx
@@ -1,15 +1,49 @@
 import React from 'react'
-
+//<div>
+//    <h1>System Login</h1>
+//    <span style={{width: '200px'}}>User:</span>
+//    <input></input>
+//    <br/>
+//    <span style={{width: '200px'}}>Password:</span>
+//    <input></input>
+//</div>
 class systemLogin extends React.Component {
+    submitClicked() {
+        alert(`email: ${$('#emailInput').val()}\npassword: ${$('#passwordInput').val()}`);
+    }
     render() {
         return (
-            <div>
-                <h1>System Login</h1>
-                <p>User:</p>
-                <input></input>
-                <p>Password:</p>
-                <input></input>
+            <div className="container">
+                <div className="row vertical-offset-100">
+                    <div className="col-md-4 col-md-offset-4">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">Please sign in</h3>
+                            </div>
+                            <div className="panel-body">
+                                <form role="form">
+                                    <fieldset>
+                                        <div className="form-group">
+                                            <input className="form-control" placeholder="E-mail" name="email"
+                                                   type="text" id="emailInput"/>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <input className="form-control" placeholder="Password" name="password"
+                                                   type="password" id="passwordInput"/>
+                                        </div>
+                                        <br/>
+                                        <br/>
+                                        <input className="btn btn-lg btn-success btn-block" onClick={this.submitClicked}
+                                               value="Login" readOnly={true}/>
+                                    </fieldset>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         )
     }
 }
