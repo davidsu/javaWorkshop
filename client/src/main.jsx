4783d6aa4462ecb000031760815290d29027f6a1
David <davidsu@wix.com>
2017-03-08 18:53:45 +0200

    rename test.jsx to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import RootComponent from './components/rootComponent.jsx'
 import ReactDom from 'react-dom'
 import React from 'react'
-
+window.activeMenu = 'login'
 ReactDOM.render(
     <RootComponent></RootComponent>,
     $('#root')[0]
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 module.exports = {
     devtool: 'source-map',
     context: path.join(__dirname),
-    entry: './src/test.jsx',
+    entry: './src/main.jsx',
     output: {
         filename: 'bundleJavaWorkshopProject.js',
         path: path.join(__dirname, 'target'),
@@ -16,13 +16,15 @@ module.exports = {
     plugins: [
         failPlugin,
         new CopyWebpackPlugin([
-            {from: 'src/index.html'}
+            {from: 'src/index.html'},
+            {context: 'src', from: '*.css'}
 
         ])
     ],
     externals: {
         react: 'React',
         'react-dom': 'ReactDOM',
+        lodash: '_',
     },
     resolve: {
         extensions: ['.js', '.jsx', '.json']
