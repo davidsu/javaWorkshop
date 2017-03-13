730e001c967ab8c29abd88dd9effdc5348f7785e
David <davidsu@wix.com>
2017-03-13 15:12:59 +0200

    use jersey 2 which has support for async stuff, use jetty server cuz it's the only way I could make jersey 2 work. Unfortunatelly still couldn't get the async stuff to work, it enters some strange loop :(

--- src/main/java/example/JAX_RS_Entry.java
+++ src/main/java/example/JAX_RS_Entry.java
@@ -1,30 +1,31 @@
 package example;
-import com.sun.jersey.api.core.PackagesResourceConfig;
-import com.sun.jersey.api.core.ResourceConfig;
-import com.sun.net.httpserver.HttpServer;
-import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import java.net.URI;
 
-import java.io.*;
+import javax.ws.rs.core.UriBuilder;
 
+import org.eclipse.jetty.server.Server;
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+/**
+ * example for an embedded http server with scala and jetty. TODO: see how to
+ * define a basic root context, without introducing boilerplate
+ */
 public class JAX_RS_Entry {
-    public static void main(String[] args) throws IOException {
-        ResourceConfig rc = new PackagesResourceConfig("example");
-        HttpServer server = HttpServerFactory.create("http://localhost:9998/", rc);
-        server.start();
+
+    public static void main(String[] args) {
+
+        URI baseUri = UriBuilder.fromUri("http://localhost/").port(9998).build();
+        ResourceConfig config = new ResourceConfig(ReceiveClientRequests.class, Users.class);
+        Server server = JettyHttpContainerFactory.createServer(baseUri, config);
         Task task = new Task(1, 2, 2, 1, 3, "11/03/2017", null, "New", true, true, false,"just a test");
         try {
             JDBC.getInstance().saveTask(task);
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        Users us = new Users();
-//        System.out.println(us.getUsers());
         System.out.println("Server running");
-        System.out.println("Visit: http://localhost:9998/helloworld");
-        System.out.println("Hit return to stop...");
-        System.in.read();
-        System.out.println("Stopping server");
-        server.stop(0);
-        System.out.println("Server stopped");
+        System.out.println("Visit: http://localhost:9998");
     }
 }
+
