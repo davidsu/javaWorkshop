921e2fd3328524e3694623d411fc2399cc5763d7
David <davidsu@wix.com>
2017-03-14 14:49:03 +0200

    refactor users.jsx to have a generic filterableTable that we will use with requests as well

---
+++ client/src/components/filterableTable.jsx
@@ -0,0 +1,137 @@
+import React from 'react'
+import _ from 'lodash'
+
+class filterableTable extends React.Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            isFiltering: false,
+            filters: _.reduce(this.props.columns, (acc, val)=> {
+                acc[val] = '';
+                return acc;
+            }, {}),
+            sort: ''
+        }
+        this.filterButtonClicked = this.filterButtonClicked.bind(this)
+        this.filter = this.filter.bind(this)
+        this.tableHeaderClicked = this.tableHeaderClicked.bind(this)
+        this.renderTableHeader = this.renderTableHeader.bind(this)
+    }
+
+    filter(evt, val) {
+        const filters = this.state.filters;
+        filters[val] = evt.nativeEvent.srcElement.value;
+        this.setState({filters})
+    }
+
+    alphabetical(a, b) {
+        var A = a.toLowerCase();
+        var B = b.toLowerCase();
+        if (A < B){
+            return -1;
+        }else if (A > B){
+            return  1;
+        }else{
+            return 0;
+        }
+    }
+
+    tableHeaderClicked(evt, key) {
+        if (!this.state.isFiltering) {
+            this.setState({sort: key})
+        }
+    }
+
+    filterButtonClicked() {
+        this.setState({isFiltering: !this.state.isFiltering})
+    }
+
+    renderTableHeader() {
+        //here filtering should be done client side, we expect to get the whole table from the server
+        const disabled = {disabled: !this.state.isFiltering}
+        return (
+            <thead>
+            <tr className="filters">
+                {_(this.props.columns)
+                    .map((val, key) => (
+                        <th key={val}
+                            onClick={evt => this.tableHeaderClicked(evt, val)}>
+                            <input {...disabled}
+                                type="text"
+                                className="form-control"
+                                placeholder={val}
+                                onKeyUp={evt => this.filter(evt, val)}/>
+                        </th>))
+                    .value()
+                }
+            </tr>
+            </thead>
+        )
+    }
+
+    renderTableBody() {
+        const self = this;
+        return (
+            <tbody>
+            {_(self.props.items)
+                .filter(item => {
+                    return _.every(item, (value = '', key = '0') => {
+                        return value.indexOf(self.state.filters[key]) > -1})
+                })
+                .thru(arr => {
+                    const sortKey = self.state.sort
+                    if(!sortKey){
+                        return arr
+                    }
+                    return arr.sort((a,b) => self.alphabetical(a[sortKey], b[sortKey]))
+                })
+                .map(item => (
+                    <tr key={item.id} onClick={() => self.props.onTableRowClicked(item)}>
+                        {
+                            _.map(self.props.columns, (colName, idx) => {
+                                return <td key={idx+colName+item[colName]}>{item[colName]}</td>
+                            })
+                        }
+                    </tr>
+                )).value()}
+            </tbody>
+        )
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <button className="btn btn-default text-right" style={{float: 'right'}} onClick={this.props.onAddItem}>{this.props.addItemDisplayName}</button>
+                    <div className="panel panel-primary filterable" style={{marginTop: '50px'}}>
+                        <div className="panel-heading">
+                            <h3 className="panel-title">{this.props.panelTitle}</h3>
+
+                            <div className="pull-right table-filter-btn">
+                                <button className="btn btn-default btn-xs btn-filter"
+                                        onClick={this.filterButtonClicked}><span
+                                    className="glyphicon glyphicon-filter"></span> Filter
+                                </button>
+                            </div>
+                        </div>
+                        <table className="table">
+                            {this.renderTableHeader()}
+                            {this.renderTableBody()}
+                        </table>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+filterableTable.propTypes = {
+    addItemDisplayName: React.PropTypes.string,
+    columns: React.PropTypes.array,
+    items: React.PropTypes.array,
+    onAddItem: React.PropTypes.func,
+    onTableRowClicked: React.PropTypes.func,
+    panelTitle: React.PropTypes.string
+}
+
+module.exports = filterableTable
--- client/src/components/users.jsx
+++ client/src/components/users.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import _ from 'lodash'
+import FilterableTable from './filterableTable.jsx'
 //see example here: http://bootsnipp.com/snippets/featured/panel-table-with-filters-per-column
 const columns = [
     'id',
@@ -9,106 +10,10 @@ const columns = [
     'password'
 ];
 class request extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            isFiltering: false,
-            filters: _.reduce(columns, (acc, val)=> {
-                acc[val] = '';
-                return acc;
-            }, {}),
-            sort: ''
-        }
-        this.filterButtonClicked = this.filterButtonClicked.bind(this)
-        this.filter = this.filter.bind(this)
-        this.tableHeaderClicked = this.tableHeaderClicked.bind(this)
-    }
-
-    filter(evt, val) {
-        const filters = this.state.filters;
-        filters[val] = evt.nativeEvent.srcElement.value;
-        this.setState({filters})
-    }
-
-    alphabetical(a, b) {
-        var A = a.toLowerCase();
-        var B = b.toLowerCase();
-        if (A < B){
-            return -1;
-        }else if (A > B){
-            return  1;
-        }else{
-            return 0;
-        }
-    }
-
-    tableHeaderClicked(evt, key) {
-        if (!this.state.isFiltering) {
-            this.setState({sort: key})
-        }
-    }
-
-    filterButtonClicked() {
-        this.setState({isFiltering: !this.state.isFiltering})
-    }
-
     tableRowClicked(user) {
         window.activeMenu = 'user:'
         window.store.user = user
         rootComponent.forceUpdate()
-    }
-
-    renderTableHeader() {
-        //here filtering should be done client side, we expect to get the whole table from the server
-        const disabled = {disabled: !this.state.isFiltering}
-        return (
-            <thead>
-            <tr className="filters">
-                {_(columns)
-                    .map((val, key) => (
-                        <th key={val}
-                            onClick={evt => this.tableHeaderClicked(evt, val)}>
-                            <input {...disabled}
-                                type="text"
-                                className="form-control"
-                                placeholder={val}
-                                onKeyUp={evt => this.filter(evt, val)}
-                                onBlur={evt=>{window.blur = evt; console.log(evt)}}/>
-                        </th>))
-                    .value()
-                }
-            </tr>
-            </thead>
-        )
-    }
-
-    renderTableBody() {
-        const self = this;
-        return (
-            <tbody>
-            {_(self.props.users)
-                .filter(user => {
-                    return _.every(user, (value = '', key = '0') => {
-                        return value.indexOf(self.state.filters[key]) > -1})
-                })
-                .thru(arr => {
-                    const sortKey = self.state.sort
-                    if(!sortKey){
-                        return arr
-                    }
-                    return arr.sort((a,b) => self.alphabetical(a[sortKey], b[sortKey]))
-                })
-                .map(user => (
-                <tr key={user.id} onClick={() => self.tableRowClicked(user)}>
-                    <td>{user.id}</td>
-                    <td>{user.full_name}</td>
-                    <td>{user.type}</td>
-                    <td>{user.email}</td>
-                    <td>{user.password}</td>
-                </tr>
-            )).value()}
-            </tbody>
-        )
     }
 
     addUser() {
@@ -123,25 +28,14 @@ class request extends React.Component {
     render() {
         return (
             <div className="container">
-                <div className="row">
-                    <button className="btn btn-default text-right" style={{float: 'right'}} onClick={this.addUser}>Add User</button>
-                    <div className="panel panel-primary filterable" style={{marginTop: '50px'}}>
-                        <div className="panel-heading">
-                            <h3 className="panel-title">Users</h3>
-
-                            <div className="pull-right table-filter-btn">
-                                <button className="btn btn-default btn-xs btn-filter"
-                                        onClick={this.filterButtonClicked}><span
-                                    className="glyphicon glyphicon-filter"></span> Filter
-                                </button>
-                            </div>
-                        </div>
-                        <table className="table">
-                            {this.renderTableHeader()}
-                            {this.renderTableBody()}
-                        </table>
-                    </div>
-                </div>
+                <FilterableTable
+                    onAddItem={this.addUser}
+                    addItemDisplayName='Add User'
+                    columns={columns}
+                    panelTitle='Users'
+                    onTableRowClicked={this.tableRowClicked}
+                    items={this.props.users}>
+                </FilterableTable>
             </div>
         )
     }
