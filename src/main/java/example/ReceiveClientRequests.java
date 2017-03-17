104edf8207b5009bdb9eadfc44ecbf00037b5060
David <davidsu@wix.com>
2017-03-17 20:49:48 +0200

    return 404 when unable to serve static file - is this the correct code?

--- src/main/java/example/ReceiveClientRequests.java
+++ src/main/java/example/ReceiveClientRequests.java
@@ -8,36 +8,33 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * Created by davidsu on 01/02/2017.
  */
-// The Java class will be hosted at the URI path "/helloworld"
 @Path("/")
 public class ReceiveClientRequests {
-    //todo return proper errors to client when failing
-    // The Java method will process HTTP GET requests
     @GET
-    // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces({MediaType.TEXT_HTML})
-    public InputStream getIndexHtml() {
+    public Response getIndexHtml() {
         return getStaticFile("index.html");
     }
 
     @GET
     @Path("/static/{fileName : (.+/?)+(html|js|css|map)}")
-    public InputStream getStaticFile(@PathParam("fileName") String fileName) {
-        System.out.println("../" + fileName);
-        URL url = getClass().getResource("../" + fileName);
-        File file = new File(url.getPath());
+    public Response getStaticFile(@PathParam("fileName") String fileName) {
         try {
-            return new FileInputStream(file);
-        } catch (FileNotFoundException e) {
+            System.out.println("../" + fileName);
+            URL url = getClass().getResource("../" + fileName);
+            File file = new File(url.getPath());
+            return Response.ok(new FileInputStream(file)).build();
+        } catch (Exception e) {
             //todo send 404
             System.out.println("exception");
             e.printStackTrace();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
-        return null;
     }
 
 }
