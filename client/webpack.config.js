84df12f6622817e804e8efeebd5dccca5a2fd362
David <davidsu@wix.com>
2017-03-21 23:56:06 +0200

    improve build time by using moment from cdn

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
         react: 'React',
         'react-dom': 'ReactDOM',
         lodash: '_',
+        moment: 'moment'
     },
     resolve: {
         extensions: ['.js', '.jsx', '.json']
