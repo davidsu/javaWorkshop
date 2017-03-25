2ab2031e57bcf2864952711010f65d52979e2a07
David <davidsu@wix.com>
2017-03-25 23:25:11 +0300

    ignore specific files in countlinesofcode

--- countLinesOfCode.sh
+++ countLinesOfCode.sh
@@ -1,2 +1,2 @@
 #/bin/sh
-find . -type f | egrep -v 'target|resources|node_modules|idea|\.git|DS_Store|./javaWorkshopProject.iml' | xargs wc -l
+find . -type f | egrep -v 'target|resources|node_modules|idea|\.git|DS_Store|./javaWorkshopProject.iml|./javaWorkshopLog.txt|./Session.vim' | xargs wc -l
