ee8da1e844f3e22df8919f8424fcf8ef80f83ff3
Alon Dangur <alondx2@gmail.com>
2017-05-15 22:16:59 +0300

    file upload test

--- src/main/java/Server/Tasks.java
+++ src/main/java/Server/Tasks.java
@@ -1,12 +1,16 @@
 package Server;
 
+import com.sun.jersey.core.header.FormDataContentDisposition;
 import org.w3c.dom.Document;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.logging.Logger;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 /*
     Class for handling Task requests
@@ -138,5 +142,51 @@ public class Tasks {
 //                    .entity(String.format("Couldn't delete task id = %1s", id))
 //                    .build();
         }
+    }
+
+    @POST
+    @Path("/fileupload")  //Your Path or URL to call this service
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+            @DefaultValue("true") @FormDataParam("enabled") boolean enabled,
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail) {
+        //Your local disk path where you want to store the file
+        String uploadedFileLocation = "./uploadedFiles/" + fileDetail.getFileName();
+        System.out.println(uploadedFileLocation);
+        // save it
+        File  objFile=new File(uploadedFileLocation);
+        if(objFile.exists())
+        {
+            objFile.delete();
+
+        }
+
+        saveToFile(uploadedInputStream, uploadedFileLocation);
+
+        String output = "File uploaded via Jersey based RESTFul Webservice to: " + uploadedFileLocation;
+
+        return Response.status(200).entity(output).build();
+
+    }
+    private void saveToFile(InputStream uploadedInputStream,
+                            String uploadedFileLocation) {
+
+        try {
+            OutputStream out = null;
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            out = new FileOutputStream(new File(uploadedFileLocation));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
+            }
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
     }
 }
